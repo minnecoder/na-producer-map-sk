ce1977c7c9df7ac8fd3d3773f69f123f95f00fc7
Sean Parkin <sdparkin@protonmail.com>
2023-08-07 22:36:13 -0500

    Created User type

---
+++ src/lib/components/types.d.ts
@@ -0,0 +1,17 @@
+export interface User {
+	name: string;
+	email: string;
+	password: string;
+	linkText: string;
+	lat: number;
+	long: number;
+	peerage?: string;
+	phone?: string;
+	facebook?: string;
+	twitter?: string;
+	instagram?: string;
+	linkedin?: string;
+	youtube?: string;
+	nasocial?: string;
+	website?: string;
+}
