990c9bdef07db6e09253ead131cfc22b38e152bb
Sean Parkin <sdparkin@protonmail.com>
2023-08-19 18:43:20 -0500

    Changed the typed of stores

--- src/lib/Stores.ts
+++ src/lib/Stores.ts
@@ -1,8 +1,6 @@
-import type { LatLngExpression } from 'leaflet';
+import type { LatLngTuple } from 'leaflet';
 import { writable } from 'svelte/store';
 
-export const indexDraggableLocation = writable<LatLngExpression>([0.0, 0.0]);
-export const registerDraggableLocation = writable<LatLngExpression>([0.0, 0.0]);
-export const findProducersDraggableLocation = writable<LatLngExpression>([
-	0.0, 0.0
-]);
+export const indexDraggableLocation = writable<LatLngTuple>([0.0, 0.0]);
+export const registerDraggableLocation = writable<LatLngTuple>([0.0, 0.0]);
+export const findProducersDraggableLocation = writable<LatLngTuple>([0.0, 0.0]);
