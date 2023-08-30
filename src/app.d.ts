b55199aa3c016719864192bfc35e059cb9126f67
Sean Parkin <sdparkin@protonmail.com>
2023-08-30 00:14:36 -0500

    Added locals.user types

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,7 +3,25 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			user: {
+				name: string;
+				email: string;
+				linkText: string;
+				role: string;
+				lat: LatLngTuple;
+				long: 0.0;
+				peerage: string;
+				phone: string;
+				facebook: string;
+				twitter: string;
+				instagram: string;
+				linkedin: string;
+				youtube: string;
+				nasocial: string;
+				website: string;
+			};
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
