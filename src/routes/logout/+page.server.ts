b13da742a532aba19d15e49145c211e94de0cb87
Sean Parkin <sdparkin@protonmail.com>
2023-08-30 00:20:09 -0500

    Created route to logout user

---
+++ src/routes/logout/+page.server.ts
@@ -0,0 +1,10 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from '../$types';
+
+export const load: PageServerLoad = async ({ cookies }) => {
+	cookies.set('session', '', {
+		path: '/',
+		expires: new Date(0)
+	});
+	throw redirect(302, '/');
+};
