5df32fd90349a9949b84d9f64c63dd5e8b22539b
Sean Parkin <sdparkin@protonmail.com>
2023-09-01 18:19:16 -0500

    Created about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,103 @@
-<h1>This is the about page</h1>
+<main>
+	<h1>About Our Project: No Agenda Producer Map</h1>
+
+	<p>
+		Welcome to the No Agenda Producer Map project, a unique platform dedicated
+		to connecting No Agenda podcast producers from all around the world. Our
+		mission is to bring together like-minded individuals who share a passion for
+		critical thinking, media deconstruction, and the pursuit of unfiltered
+		truth.
+	</p>
+
+	<h3>Our Vision</h3>
+
+	<p>
+		At No Agenda Producer Map, we aim to foster a sense of community and
+		camaraderie among the global audience of the No Agenda podcast, hosted by
+		Adam Curry and John C. Dvorak. We recognize that the No Agenda show has a
+		diverse and dedicated fan base, and our platform seeks to strengthen these
+		connections by visualizing the geographic distribution of No Agenda
+		producers.
+	</p>
+
+	<h3>Why Mapping?</h3>
+
+	<p>
+		We believe that knowing where fellow No Agenda producers are located can be
+		both fascinating and informative. It's an opportunity to understand the
+		show's impact on a global scale and to appreciate the diversity of its
+		audience. By mapping the locations of No Agenda producers, we can also
+		facilitate meetups, discussions, and networking among fans who might live
+		nearby.
+	</p>
+
+	<h3>How It Works</h3>
+
+	<p>
+		Our platform allows No Agenda producers to create user profiles and
+		voluntarily share their locations on an interactive map. You can see how
+		many producers are situated within a specified distance from your own
+		location. Whether you're interested in finding local meetups, connecting
+		with nearby fans, or simply exploring the global reach of the No Agenda
+		community, our map has you covered.
+	</p>
+
+	<h3>Privacy and Security</h3>
+
+	<p>
+		We take your privacy and security seriously. We only collect location data
+		if you choose to share it voluntarily. Your personal information is
+		protected, and we do not share it with third parties. You have full control
+		over the visibility of your location on the map.
+	</p>
+
+	<h3>Join Us!</h3>
+
+	<p>
+		We invite all No Agenda producers to join our community and contribute to
+		our map. Together, we can create a dynamic and engaging representation of
+		the global No Agenda audience. Whether you're a longtime producer or just
+		discovering the show, we welcome you to become a part of this unique
+		project.
+	</p>
+
+	<h3>Get Started Today</h3>
+
+	<p>
+		Ready to explore the global No Agenda community? Join us today and start
+		mapping your No Agenda location! Connect with fellow fans, discover new
+		perspectives, and be a part of this exciting journey.
+	</p>
+
+	<p>
+		Thank you for being a part of the No Agenda Producer Map project. We look
+		forward to seeing the No Agenda community come together in new and exciting
+		ways!
+	</p>
+
+	<p>
+		If you have any questions or suggestions, please don't hesitate to contact
+		us at <a href="mailto:sdparkin@protonmail.com">sdparkin@protonmail.com</a>.
+		Together, let's keep the No Agenda conversation going, one map marker at a
+		time.
+	</p>
+</main>
+
+<style>
+	main {
+		padding: 2rem 0;
+		margin: 0 auto;
+		width: 70%;
+	}
+	h1 {
+		margin: 1rem 0;
+		text-align: center;
+	}
+	h3 {
+		margin: 1rem 0;
+	}
+
+	a {
+		text-decoration: none;
+	}
+</style>
