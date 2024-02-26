f56cfa05d26d92c00a9d2b30e0a2408336148173
Sean Parkin <sdparkin@protonmail.com>
2024-02-25 21:44:34 -0600

    Added external link to blue marker image

--- src/lib/components/Map/Marker.svelte
+++ src/lib/components/Map/Marker.svelte
@@ -8,6 +8,12 @@
 	let marker: L.Marker | undefined;
 	let markerElement: HTMLElement;
 
+const markerIcon = L.icon({
+	iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png`,
+		shadowUrl: `https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png`
+})
+
+
 	const { getMap }: { getMap: () => L.Map | undefined } = getContext('map');
 	const map = getMap();
 
@@ -20,6 +26,7 @@
 		if (map) {
 			marker = L.marker([lat, long]).addTo(map);
 		}
+		marker?.setIcon(markerIcon)
 	});
 
 	onDestroy(() => {
